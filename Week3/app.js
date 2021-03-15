247c49b1757d66f2105e4224c2fcf74d857d873d
yayay927 <32444273+yayay927@users.noreply.github.com>
2021-03-15 17:22:54 +0800

    solve problem of number display

--- Week3/app.js
+++ Week3/app.js
@@ -15,12 +15,14 @@ app.get("/getData", (req, res, next) => {
 
 	let n = parseInt(req.query.number);
 	
+	
 	if (req.query.number === undefined){
 		res.send("Lack of Parameter");
 	} else if (isNaN(req.query.number)){
 		res.send("Wrong Parameter");
 	} else if ( n > 0) {
-		res.send((n*(n+1))/2);
+		let result = n*(n+1)/2;
+		res.send(`${result}`); //result.toString //${result}
 	}
 	else {
 		res.send("Wrong Parameter");	
@@ -31,7 +33,10 @@ app.listen(3000);
 
 module.exports = app;
 
-
+//another way
+// else if ( req.query.number > 0) {
+// 	res.send(Number(req.query.number)*(Number(req.query.number)+1)/2);
+// }
 
 
 
