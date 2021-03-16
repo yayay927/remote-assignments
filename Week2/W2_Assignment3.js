945924c62707afdcf1d37674efa2f684ac20b5ea
yayay927 <32444273+yayay927@users.noreply.github.com>
2021-03-17 01:40:41 +0800

    2021/03/10 revised

--- Week2/W2_Assignment3.js
+++ Week2/W2_Assignment3.js
@@ -1,6 +1,34 @@
 function avg(data){
   // your code here
-  const averageNumber = (${data[0]['price']} + ${data[1]['price']} + ${data[2]['price']})/3
+  const averageNumber = (
+    data.products[0].price +
+    data.products[1].price +
+    data.products[2].price
+    )/3
+
+  // show the average price of all products
+  return averageNumber;
+}
+
+let data = {
+    size:3,products:[
+      {name:"Product 1",price:100},
+      {name:"Product 2",price:700},
+      {name:"Product 3",price:250}
+    ]
+};
+
+avg(data);
+
+
+
+
+
+//first version
+/*
+function avg(data){
+  // your code here
+  const averageNumber = (`${data[0]['price']}` + `${data[1]['price']}` + `${data[2]['price']}`)/3
 }
 avg({
   size:3,products:[
@@ -11,3 +39,22 @@ avg({
 });
   // show the average price of all products
 return averageNumber;
+*/
+
+//second version
+/*
+function avg(data){
+  // your code here
+  const averageNumber = (`${data[0]['price']}` + `${data[1]['price']}` + `${data[2]['price']}`)/3
+
+avg({
+  size:3,products:[
+    {name:"Product 1",price:100},
+    {name:"Product 2",price:700},
+    {name:"Product 3",price:250}
+  ]
+});
+  // show the average price of all products
+return averageNumber;
+}
+*/
